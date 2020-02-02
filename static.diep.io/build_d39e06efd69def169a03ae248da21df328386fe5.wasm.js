9dd7ea1e34fc5dd399a0e30e681f52632e25ce35
Colonizor48 <43299519+Colonizor48@users.noreply.github.com>
2020-02-02 15:54:40 -0500

    Create build_d39e06efd69def169a03ae248da21df328386fe5.wasm.js
    Its a mysterious file i found written in webasembly while i was poking around with inspect element
    It may be of use to us
    Within the file is the link to the other file
    I couldnt copypaste it as it was even more unreadable sorry

---
+++ static.diep.io/build_d39e06efd69def169a03ae248da21df328386fe5.wasm.js
@@ -0,0 +1,7 @@
+https://static.diep.io/build_d39e06efd69def169a03ae248da21df328386fe5.wasm.js
+
+Link for a file i found while poking around with inspect element
+It seems to be written in webassembly so we would need to decompile it
+It might be helpful idk what it is tho.
+I suspect it may be part of the server however due to build in the title and the hexadecimal thing might be a build version/id?
+prehaps it is simply part of the client
